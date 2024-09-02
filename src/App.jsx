100042f6518727d0d2607b39f86d8c8c4755cca8
DAgustinAguilera <danielagustinaguilera@gmail.com>
2024-09-02 01:13:15 -0300

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { FormContainer } from './components/FormContainer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './components/NavBar';
-import Inicio from './components/Inicio';
-import Informes from './components/Informes';
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { FormContainer } from "./components/FormContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
+import Inicio from "./components/Inicio";
+import Login from "./components/Login"
+import Informes from "./components/Informes";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
+        <Route path="/login" element={<Login/>} />
         <Route path="/" element={<Inicio />} />
         <Route path="/reportes" element={<FormContainer />} />
-        <Route path="/informes" element={<Informes/>} />
+        <Route path="/informes" element={<Informes />} />
       </Routes>
     </Router>
   );
