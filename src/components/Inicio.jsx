13688dd23ff981cb584f5cb0f105bb5689c634ce
DAgustinAguilera <agustindanielaguilera@gmail.com>
2024-09-02 04:59:38 -0300

    cambios

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,8 +4,8 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Swal from 'sweetalert2'
 
-const BASE_BACKEND_URL = import.meta.env.BASE_BACKEND_URL || "http://localhost:4000";
-console.log(BASE_BACKEND_URL, import.meta.env.VITE_VERCEL_ENV)
+const LOCAL_BASE_BACKEND_URL = import.meta.env.BASE_BACKEND_URL || "http://localhost:4000";
+console.log(LOCAL_BASE_BACKEND_URL, import.meta.env.VITE_VERCEL_ENV)
 const Inicio = () => {
   const [reportes, setReportes] = useState([]);
 
