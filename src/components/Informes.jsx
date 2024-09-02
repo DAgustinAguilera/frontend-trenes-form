2e6b64dbb80f44936e2f302ba06bf5dda2bb3793
DAgustinAguilera <agustindanielaguilera@gmail.com>
2024-09-02 05:40:53 -0300

    cambios

--- src/components/Informes.jsx
+++ src/components/Informes.jsx
@@ -17,43 +17,25 @@ import {
             <h1 className="mb-4">PDF Generator</h1>
           </CardHeader>
           <CardBody>
-            <Form onSubmit={handleSubmit}>
+            <Form>
               <Form.Group>
                 <Form.Label htmlFor="desarrolle">Desarrolle</Form.Label>
                 <Form.Control
-                  as="textarea"
-                  id="desarrolle"
-                  rows={5}
-                  value={formData.desarrolle}
-                  onChange={handleChange}
                 />
               </Form.Group>
               <Form.Group className="mb-3">
                 <Form.Label htmlFor="name">Name</Form.Label>
                 <Form.Control
-                  type="text"
-                  id="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  placeholder="Enter your name"
                 />
               </Form.Group>
               <Form.Group className="mb-3">
                 <Form.Label htmlFor="dia">Dia</Form.Label>
                 <Form.Control
-                  type="text"
-                  id="dia"
-                  value={formData.dia}
-                  onChange={handleChange}
                 />
               </Form.Group>
               <Form.Group className="mb-3">
                 <Form.Label htmlFor="mes">Mes</Form.Label>
                 <Form.Control
-                  type="text"
-                  id="mes"
-                  value={formData.mes}
-                  onChange={handleChange}
                 />
               </Form.Group>
               <Button type="submit" variant="primary">
